56b537a567fd6e2289529e80c3e926cbb4677c8c
Steve <contact@stevedylan.dev>
2026-04-08 19:46:20 -0400

    chore: enabled footnotes for markdown

--- apps/posts/src/server.rs
+++ apps/posts/src/server.rs
@@ -316,6 +316,7 @@ fn render_markdown(content: &str) -> String {
     options.insert(Options::ENABLE_STRIKETHROUGH);
     options.insert(Options::ENABLE_TABLES);
     options.insert(Options::ENABLE_TASKLISTS);
+    options.insert(Options::ENABLE_FOOTNOTES);
     let parser = Parser::new_ext(content, options);
     let mut html_output = String::new();
     html::push_html(&mut html_output, parser);
